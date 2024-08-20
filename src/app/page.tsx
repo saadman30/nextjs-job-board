10a8bfaec9d357a97de21ba6a7dc0e179dbd2f37
saadman30 <saadman.ofc@gmail.com>
2024-08-20 22:01:16 +0600

    search functionality added in jobresults

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
 import JobFilterSidebar from "@/components/JobFilterSidebar";
 import H1 from "@/components/ui/h1";
 import JobResults from "@/components/JobResults";
+import { PageProps } from "../../.next/types/app/layout";
+import { JobFilterValues } from "@/lib/validation";
 
-export default async function Home() {
-  
+export default async function Home({
+  searchParams: { q, type, location, remote },
+}: PageProps) {
+  const filterValues: JobFilterValues = {
+    q,
+    type,
+    location,
+    remote: remote === true,
+  };
 
   return (
     <main className="m-auto my-10 max-w-5xl space-y-10 px-3">
@@ -13,7 +22,7 @@ export default async function Home() {
       </div>
       <section className="flex flex-col gap-4 md:flex-row">
         <JobFilterSidebar />
-        <JobResults />
+        <JobResults filterValues={filterValues} />
       </section>
     </main>
   );
